12e4b4643f428e76d6310870999cc497e7c7a0aa
AmauryPigeat <120026100+AmauryPigeat@users.noreply.github.com>
2025-03-28 10:11:24 +0100

    structure principale

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 import pygame
+from player import Player
 
 class Game():
     def __init__(self, window_size):
@@ -15,8 +16,9 @@ class Game():
         self.power_ups = None # Liste des power-ups
 
     def setup(self):
+
+        self.player = Player(self.screen, 50)
         """
-        self.player = ...
         self.platforms = ...
         self.enemies = ...
         self.power_ups = ...
@@ -25,17 +27,31 @@ class Game():
 
     def run(self):
         while self.isRunning:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    self.isRunning = False
+            # Structure du code : https://www.youtube.com/watch?v=N56R1V5XZBw&list=PLKeQQTikvsqkeJlhiE8mXwskOhXLKdl8m&index=3
+            # Gestion de l'évenement "Quit
+            self.handling_events()
 
-            self.screen.fill("black")
+            # Mise à jour des différents éléments
+            self.update()
 
-            # Mise à jour et rendu des entités
+            # Rendu des entités
+            self.display()
 
-            pygame.display.flip()
             self.dt = self.clock.tick(60) / 1000
 
         pygame.quit()
+
+    def handling_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.isRunning = False
+
+    def update(self):
+        self.player.move()
+
+    def display(self):
+        self.screen.fill("black")
+        self.player.draw()
+        pygame.display.flip()
 
 
