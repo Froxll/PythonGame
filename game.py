f0790bdbaed8768be8a4d74f3a57149e5c7e0cb9
Nathan <nathan@gadbin.com>
2025-03-31 00:24:06 +0200

    Golem spawning on map

--- game.py
+++ game.py
@@ -16,11 +16,22 @@ class Game():
         self.platforms = None  # Liste des plateformes
         self.power_ups = None  # Liste des power-ups
 
+        self.background = pygame.image.load("img/Map.png")
+        self.background = pygame.transform.scale(self.background, self.screen.get_size())  # Ajuste à la taille de l'écran
+
+        self.full_map = pygame.image.load("img/Map.png")
+
+        self.camera_x = 0
+        self.camera_y = 0
+
+        # Taille de la fenêtre
+        self.window_width, self.window_height = window_size
+
         self.all_monsters = pygame.sprite.Group()
         self.spawn_monster()
 
     def spawn_monster(self):
-        monster = Monster()
+        monster = Monster(1260, 1785, 1260, 1620)  # Spawn en 200 / 300 et va de 200 à 400 en x
         self.all_monsters.add(monster)
 
     def setup(self):
@@ -53,12 +64,36 @@ class Game():
             if event.type == pygame.QUIT:
                 self.isRunning = False
 
+        # Vérifier les touches pressées
+        keys = pygame.key.get_pressed()
+        speed = 10  # Vitesse de déplacement de la caméra
+
+        # Déplacer la caméra selon les touches fléchées
+        if keys[pygame.K_LEFT]:
+            self.camera_x = max(0, self.camera_x - speed)  # Empêcher d'aller en dehors de la map
+        if keys[pygame.K_RIGHT]:
+            self.camera_x = min(5755 - self.window_width, self.camera_x + speed)  # Empêcher d'aller trop loin
+        if keys[pygame.K_UP]:
+            self.camera_y = max(0, self.camera_y - speed)
+        if keys[pygame.K_DOWN]:
+            self.camera_y = min(2164 - self.window_height, self.camera_y + speed)
+
     def update(self):
         # Mettre à jour tous les monstres avec le delta time (dt)
         self.all_monsters.update(self.dt)
 
     def display(self):
-        self.screen.fill("black")
-        self.all_monsters.draw(self.screen)
-        # self.player.draw()
+        # Découper la portion de la map visible selon la position de la caméra
+        map_view = self.full_map.subsurface((self.camera_x, self.camera_y, self.window_width, self.window_height))
+
+        # Dessiner la portion de la map
+        self.screen.blit(map_view, (0, 0))
+        
+        # Dessiner les monstres
+        for monster in self.all_monsters:
+            # Décaler la position du monstre selon la position de la caméra
+            display_x = monster.rect.x - self.camera_x
+            display_y = monster.rect.y - self.camera_y
+            self.screen.blit(monster.image, (display_x, display_y))
+
         pygame.display.flip()
--- monster.py
+++ monster.py
@@ -3,69 +3,56 @@ import pygame
 
 class Monster(pygame.sprite.Sprite):
 
-    def __init__(self):
+    def __init__(self, x, y, left_limit, right_limit):
         super().__init__()
+
         self.health = 100
         self.max_health = 100
         self.attack = 5
 
-        # Charger les 18 images du golem
+        # Charger les images du golem
         self.images_normal = []
-        self.images_flipped = []  # Liste pour les images inversées
+        self.images_flipped = []  
 
-        for i in range(18):  # Tes images vont de 0 à 17
+        for i in range(18):
             img_path = f'img/Golem/Walking/Golem_01_Walking_{i:03d}.png'
             img = pygame.image.load(img_path)
-
-            # Ajouter l'image normale à la liste
             self.images_normal.append(pygame.transform.scale(img, (100, 100)))
-
-            # Créer l'image miroir et l'ajouter à la liste des images inversées
-            flipped_img = pygame.transform.flip(img, True, False)  # Inverser horizontalement
+            flipped_img = pygame.transform.flip(img, True, False)
             self.images_flipped.append(pygame.transform.scale(flipped_img, (100, 100)))
 
-        # Initialiser l'image actuelle
-        self.image = self.images_normal[0]
+        # Initialiser l'image du golem
+        self.image = self.images_normal[0]  # Prendre la première image pour initialiser
         self.rect = self.image.get_rect()
+        self.rect.x = x  # Position en X
+        self.rect.y = y  # Position en Y
 
-        # Position initiale du golem
-        self.rect.x = 100
-        self.rect.y = 500
+        # Limites du mouvement
+        self.left_limit = left_limit
+        self.right_limit = right_limit
 
-        # Variables pour alterner les images
-        self.current_image = 0  # L'indice de l'image actuelle
-        self.time_since_last_update = 0  # Temps depuis la dernière mise à jour de l'image
-        self.animation_speed = 0.1  # Intervalle de temps entre chaque changement d'image (en secondes)
-
-        # Direction du monstre (True = droite, False = gauche)
+        # Direction et vitesse
         self.facing_right = True
-        self.speed = 2  # Vitesse du déplacement
+        self.speed = 2  
+
+        # Animation
+        self.current_image = 0
+        self.time_since_last_update = 0
+        self.animation_speed = 0.1  
 
     def update(self, dt):
-        # Mise à jour du temps écoulé
+    # Mise à jour de l'animation
         self.time_since_last_update += dt
-
-        # Si le temps écoulé depuis la dernière mise à jour est supérieur à l'intervalle défini
         if self.time_since_last_update >= self.animation_speed:
-            # Réinitialiser le temps
             self.time_since_last_update = 0
-
-            # Passer à l'image suivante
             self.current_image = (self.current_image + 1) % len(self.images_normal)
+            self.image = self.images_normal[self.current_image] if self.facing_right else self.images_flipped[self.current_image]
 
-            # Mettre à jour l'image du golem en fonction de la direction
-            if self.facing_right:
-                self.image = self.images_normal[self.current_image]
-            else:
-                self.image = self.images_flipped[self.current_image]
+        # Déplacement du monstre
+        self.rect.x += self.speed if self.facing_right else -self.speed  
 
-        # Déplacer le monstre
-        self.rect.x += self.speed if self.facing_right else -self.speed  # Déplacement dans le bon sens
-
-        # Si le monstre atteint la droite de l'écran, il fait demi-tour
-        if self.rect.x > 1280 - self.rect.width:  # La largeur de l'écran - largeur du monstre
-            self.facing_right = False  # Inverser la direction
-
-        # Si le monstre atteint la gauche de l'écran, il fait demi-tour
-        elif self.rect.x < 0:
-            self.facing_right = True  # Inverser la direction
+        # Changer de direction aux limites
+        if self.rect.x >= self.right_limit:
+            self.facing_right = False
+        elif self.rect.x <= self.left_limit:
+            self.facing_right = True
