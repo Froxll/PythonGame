47a60088ac86e83d59ed3ba3984cdabb2b179bad
Froxll <119114922+Froxll@users.noreply.github.com>
2025-03-31 11:45:40 +0200

    golemos sont animés quand ils arrêtent ed bouger

--- monster.py
+++ monster.py
@@ -12,17 +12,26 @@ class Monster(pygame.sprite.Sprite):
         self.attack = 5
 
         # On créé les tableaux pour stocker les images du golem
-        self.images_normal = []
-        self.images_flipped = []
+        self.images_walking_normal = []
+        self.images_walking_flipped = []
+        self.images_idle_normal = []
+        self.images_idle_flipped = []
 
         for i in range(18):  # On stock les images d'animations du golem en version normale et en mirror
             img_path = f'img/Golem/Walking/Golem_01_Walking_{i:03d}.png'
             img = pygame.image.load(img_path)
-            self.images_normal.append(pygame.transform.scale(img, (150, 150)))
+            self.images_walking_normal.append(pygame.transform.scale(img, (150, 150)))
             flipped_img = pygame.transform.flip(img, True, False)
-            self.images_flipped.append(pygame.transform.scale(flipped_img, (150, 150)))
+            self.images_walking_flipped.append(pygame.transform.scale(flipped_img, (150, 150)))
 
-        self.image = self.images_normal[0]  # On prend la première image pour initialiser notre golem
+        for i in range(8):
+            img_path = f'img/Golem/Idle/Golem_01_Idle_{i:03d}.png'
+            img = pygame.image.load(img_path)
+            self.images_idle_normal.append(pygame.transform.scale(img, (150, 150)))
+            flipped_img = pygame.transform.flip(img, True, False)
+            self.images_idle_flipped.append(pygame.transform.scale(flipped_img, (150, 150)))
+
+        self.image = self.images_walking_normal[0]  # On prend la première image pour initialiser notre golem
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
@@ -33,41 +42,51 @@ class Monster(pygame.sprite.Sprite):
 
         # Direction et vitesse
         self.facing_right = True
-        self.speed = 2
+        self.speed = 1
 
         # Animation
         self.current_image = 0
         self.time_since_last_update = 0
-        self.animation_speed = 0.1
+        self.animation_speed = 0.3
 
-        #Pause
-        self.is_stopped = False  # Indique si le golem est à l'arrêt
+        # Pause
+        self.is_stopped = False
+        self.is_idle = False
         self.stop_timer = 0  # Temps restant d'arrêt
-        self.next_stop_time = random.uniform(2, 5)  # Prochaine pause dans 2 à 5 sec
+        self.next_stop_time = random.uniform(2, 5)  # Prochaine pause entre 2 à 5 sec
 
     def update(self, dt):
-        # Mise à jour de l'animation
         self.time_since_last_update += dt
         self.next_stop_time -= dt
 
+        # Gérer l'animation walking
         if not self.is_stopped and self.time_since_last_update >= self.animation_speed:
             self.time_since_last_update = 0
-            self.current_image = (self.current_image + 1) % len(self.images_normal)
-            self.image = self.images_normal[self.current_image] if self.facing_right else self.images_flipped[
-                self.current_image]
+            self.current_image = (self.current_image + 1) % len(self.images_walking_normal)
+            self.image = self.images_walking_normal[self.current_image] if self.facing_right else \
+                self.images_walking_flipped[self.current_image]
 
+        # Gérer la pause du golem
         if self.next_stop_time <= 0 and not self.is_stopped:
             self.is_stopped = True
-            self.stop_timer = 1
+            self.stop_timer = 2
 
+        # Gérer l'animation idle quand le golem est arrêté
         if self.is_stopped:
+            self.time_since_last_update += dt
+            if self.time_since_last_update >= self.animation_speed:
+                self.time_since_last_update = 0
+                self.current_image = (self.current_image + 1) % len(self.images_idle_normal)
+                self.image = self.images_idle_normal[self.current_image] if self.facing_right else \
+                    self.images_idle_flipped[self.current_image]
+
             self.stop_timer -= dt
             if self.stop_timer <= 0:
                 self.is_stopped = False
                 self.next_stop_time = random.uniform(2, 5)
-            return  # On sort de update pour ne pas déplacer le golem pendant la pause
+            return
 
-        # Déplacement du monstre à droite ou à gauche
+        # Déplacement du golem à droite ou à gauche
         if self.facing_right:
             self.rect.x += self.speed
         else:
