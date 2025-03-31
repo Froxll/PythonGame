fe878003b84471133fe6c620dcf4e9e07b00fcab
Ethan HEURTIN <ethan.heurtin@outlook.com>
2025-03-31 09:58:40 +0200

    Map with textures and rect

--- game.py
+++ game.py
@@ -1,4 +1,7 @@
 import pygame
+import pytmx
+
+
 ##from player import Player
 
 class Game():
@@ -15,14 +18,31 @@ class Game():
         self.enemies = None   # Liste des ennemis
         self.power_ups = None # Liste des power-ups
 
-        self.image = pygame.image.load('img/Map.jpg').convert()
 
+
+        self.background = pygame.image.load("img/Map.png").convert()
+        tmx_data = pytmx.load_pygame("data/MapTMX.tmx")
+
+        self.rect_list = []
+        self.ladder_list = []
+
+        for obj in tmx_data.objects:
+            if obj.name == "plateforme":
+                rect = pygame.Rect(obj.x, obj.y, obj.width, obj.height)
+                self.rect_list.append(rect)
+            if obj.name == "echelle":
+                rect = pygame.Rect(obj.x, obj.y, obj.width, obj.height)
+                self.ladder_list.append(rect)
+
+        # Caméra temporaire Et1
+        self.camera_x = 0
+        self.camera_y = 0
+        self.camera_speed = 900
 
     def setup(self):
 
-        self.player = Player(self.screen, 50)
+        #self.player = Player(self.screen, 50)
         """
-        self.player = ...
         self.platforms = ...
         self.enemies = ...
         self.power_ups = ...
@@ -30,9 +50,11 @@ class Game():
 
 
     def run(self):
+
         while self.isRunning:
             # Structure du code : https://www.youtube.com/watch?v=N56R1V5XZBw&list=PLKeQQTikvsqkeJlhiE8mXwskOhXLKdl8m&index=3
             # Gestion de l'évenement "Quit
+
             self.handling_events()
 
             # Mise à jour des différents éléments
@@ -51,11 +73,32 @@ class Game():
                 self.isRunning = False
 
     def update(self):
-        self.player.move()
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_f]:
+            self.camera_x -= self.camera_speed * self.dt
+        if keys[pygame.K_h]:
+            self.camera_x += self.camera_speed * self.dt
+        if keys[pygame.K_t]:
+            self.camera_y -= self.camera_speed * self.dt
+        if keys[pygame.K_g]:
+            self.camera_y += self.camera_speed * self.dt
 
     def display(self):
-        self.screen.fill("black")
-        self.player.draw()
+        # self.player.draw()
+
+        self.screen.blit(self.background, (-self.camera_x, -self.camera_y))
+
+        for rect in self.rect_list:
+            shifted_rect = rect.move(-self.camera_x, -self.camera_y)
+            pygame.draw.rect(self.screen, (255, 0, 0), shifted_rect, width=4)
+        
+        for rect in self.ladder_list:
+            shifted_rect = rect.move(-self.camera_x, -self.camera_y)
+            pygame.draw.rect(self.screen, (255, 0, 0), shifted_rect, width=4)
+
+
+
+
         pygame.display.flip()
 
 
--- main.py
+++ main.py
@@ -3,5 +3,4 @@ from game import Game
 
 if __name__ == "__main__":
     game = Game((1280, 720))
-    game.setup()
     game.run()
